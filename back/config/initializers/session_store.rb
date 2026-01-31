c8992b0eb0083ac8780bfc0ffec82154b7663ec6
EnHoraBuena-Boca <matiasxav@proton.me>
2026-01-31 16:46:52 -0500

    Messing with cookies

--- back/config/initializers/session_store.rb
+++ back/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
   key: '_your_app_session',
-  same_site: :none,
+  same_site: Rails.env.production? ? :none : :lax,
   secure: Rails.env.production?,
-  expire_after: 1.hour
+  domain: :all
