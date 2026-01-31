bf972b962b96efcdee048228551255290b579bdd
EnHoraBuena-Boca <matiasxav@proton.me>
2026-01-31 17:08:41 -0500

    crashed fix

--- back/config/application.rb
+++ back/config/application.rb
@@ -33,7 +33,7 @@ module Back
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
   end
 end
--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -26,19 +26,13 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  
-  config.ssl_options = {
-      cookie_configuration: {
-        secure: true,
-        same_site: :none
-      }
-    }
-  
-  # ALSO ADD THIS to ensure the session store is forced:
+  config.action_dispatch.cookies_same_site_protection = :none
+
   config.session_store :cookie_store, 
     key: '_your_app_session', 
     same_site: :none, 
     secure: true
+  
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
