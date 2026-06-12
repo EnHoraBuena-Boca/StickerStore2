f086172a064641a60fabaa4849fe42229e3f18ff
AustinP0027 <austinp0027@gmail.com>
2026-06-12 16:19:59 -0500

    Change Rarity Logic

--- back/app/models/pack_creation.rb
+++ back/app/models/pack_creation.rb
@@ -1,20 +1,38 @@
 class PackCreation
 
+  ## Old Rarity Logic ##
+  # def pack_rarities
+  #   pack = Array.new
+  #   for index in 0..4
+  #     rarity = rand(101)
+  #     if rarity >= 99
+  #       pack.insert(index, 3)
+  #     elsif rarity >= 95 && rarity < 99
+  #       pack.insert(index, 2)
+  #     elsif rarity <95 && rarity >=70
+  #       pack.insert(index, 1)
+  #     else
+  #       pack.insert(index, 0)
+  #     end
+  #   end
+  #   return pack
+  # end
+
+  ## New Rarity Logic ##
   def pack_rarities
-    pack = Array.new
-    for index in 0..4
-      rarity = rand(101)
-      if rarity >= 99
-        pack.insert(index, 3)
-      elsif rarity >= 95 && rarity < 99
-        pack.insert(index, 2)
-      elsif rarity <95 && rarity >=70
-        pack.insert(index, 1)
+    Array.new(5) do
+      roll = rand * 100
+
+      if roll < 0.5
+        3
+      elsif roll < 3.0
+        2
+      elsif roll < 20.0
+        1
       else
-        pack.insert(index, 0)
+        0
       end
     end
-    return pack
   end
 
 
